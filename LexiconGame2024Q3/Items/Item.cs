74ebf687e4ba0e3ad1f2b0e79a49b3fdb52b710a
Jonathan Krall <jonathan.krall@lexicon.se>
2024-09-24 13:37:33 +0200

    Added Items

---
+++ LexiconGame2024Q3/Items/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexiconGame2024Q3.Items
+{
+    internal class Item : IDrawable
+    {
+        private readonly string name;
+
+        public ConsoleColor Color { get; }
+
+        public string Symbol { get; }
+
+        public Item(string symbol, ConsoleColor color, string name)
+        {
+            Symbol = symbol;
+            Color = color;
+            this.name = name;
+        }
+
+        public override string ToString() => name; 
+    }
+}
