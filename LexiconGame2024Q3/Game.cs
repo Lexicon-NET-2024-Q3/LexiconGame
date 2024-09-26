a08eb45e229947a89c5cff279e6d8020ce1dfb5f
Jonathan Krall <jonathan.krall@lexicon.se>
2024-09-26 11:58:55 +0200

    Random placement

--- LexiconGame2024Q3/Game.cs
+++ LexiconGame2024Q3/Game.cs
@@ -56,26 +56,26 @@ internal class Game
             case ConsoleKey.DownArrow:
                 Move(Direction.South);
                 break;
-            //case ConsoleKey.P:
-            //    PickUp(); 
-            //    break;
-            //case ConsoleKey.I:
-            //    Inventory(); 
-            //    break;
+                //case ConsoleKey.P:
+                //    PickUp(); 
+                //    break;
+                //case ConsoleKey.I:
+                //    Inventory(); 
+                //    break;
         }
 
-        
+
 
         if (actionMenu.ContainsKey(keyPressed))
             actionMenu[keyPressed]?.Invoke();
-}
+    }
 
     private void Inventory()
     {
-        ConsoleUI.AddMessage(hero.BackPack.Count > 0 ? "Inventory:" : "No items"); 
+        ConsoleUI.AddMessage(hero.BackPack.Count > 0 ? "Inventory:" : "No items");
         for (int i = 0; i < hero.BackPack.Count; i++)
         {
-            ConsoleUI.AddMessage($"{i + 1}: {hero.BackPack[i]}"); 
+            ConsoleUI.AddMessage($"{i + 1}: {hero.BackPack[i]}");
         }
     }
 
@@ -84,7 +84,7 @@ internal class Game
         if (hero.BackPack.IsFull)
         {
             ConsoleUI.AddMessage("Backpack is full");
-            return; 
+            return;
         }
 
         List<Item> items = hero.Cell.Items;
@@ -94,8 +94,8 @@ internal class Game
 
         if (hero.BackPack.Add(item))
         {
-           ConsoleUI.AddMessage($"Hero picked up {item}");
-            items.Remove(item); 
+            ConsoleUI.AddMessage($"Hero picked up {item}");
+            items.Remove(item);
         }
 
 
@@ -105,17 +105,17 @@ internal class Game
     {
         Position newPosition = hero.Cell.Position + movement;
         Cell newCell = map.GetCell(newPosition);
-        if (newCell != null) hero.Cell = newCell; 
+        if (newCell != null) hero.Cell = newCell;
         //Cell newPosition = map.GetCell(y, x);
         //if (newPosition is not null) hero.Cell = newPosition;
     }
 
     private void DrawMap()
     {
-        ConsoleUI.Clear(); 
+        ConsoleUI.Clear();
         ConsoleUI.Draw(map);
-        ConsoleUI.PrintStats($"Hero's Health: {hero.Health}"); 
-        ConsoleUI.PrintLog(); 
+        ConsoleUI.PrintStats($"Hero's Health: {hero.Health}, Enemies: {map.Creatures.Count - 1}");
+        ConsoleUI.PrintLog();
     }
 
     private void Initialize()
@@ -125,20 +125,38 @@ internal class Game
             {ConsoleKey.P, PickUp},
             {ConsoleKey.I, Inventory}
         };
+
+        var r = new Random();
         //ToDo: Read from config maybe
         map = new Map(width: 10, height: 10);
         Cell heroCell = map.GetCell(0, 0);
         hero = new Hero(heroCell);
         map.Creatures.Add(hero);
 
-        map.GetCell(2, 4)?.Items.Add(Item.Coin());
-        map.GetCell(3, 7)?.Items.Add(Item.Coin());
-        map.GetCell(4, 8)?.Items.Add(Item.Stone());
-        map.GetCell(3, 7)?.Items.Add(Item.Stone());
+        RCell().Items.Add(Item.Coin());
+        RCell().Items.Add(Item.Coin());
+        RCell().Items.Add(Item.Stone());
+        RCell().Items.Add(Item.Stone());
 
-        map.Place(new Orc(map.GetCell(0, 5)!));
-        map.Place(new Orc(map.GetCell(0, 5)!));
-        map.Place(new Troll(map.GetCell(4, 5)!));
+        map.Place(new Orc(RCell()));
+        map.Place(new Troll(RCell()));
+        map.Place(new Goblin(RCell()));
+        map.Place(new Orc(RCell()));
+        map.Place(new Troll(RCell()));
+        map.Place(new Troll(map.GetCell(0,5)!));
+        map.Place(new Troll(map.GetCell(0,5)!));
+
+        Cell RCell()
+        {
+            var width = r.Next(0, map.Width);
+            var height = r.Next(0, map.Height);
+
+            var cell = map.GetCell(width, height);
+
+            ArgumentNullException.ThrowIfNull(cell, nameof(cell));
+
+            return cell;
+        }
 
     }
 }
